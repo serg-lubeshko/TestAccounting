60ec9fe27f4d837077d20e07e55e59b4546aa27a
Sergei_L <lubeshkosergei@gmail.com>
2022-11-14 21:33:18 +0300

    task_4_category:: create fixtures for download (datapp) and model Category

---
+++ accounting/management/data/сategory_data.py
@@ -0,0 +1,52 @@
+categories = [
+    {
+        "category_id": 1,
+        "category_name": "Забота о себе",
+        "user_id": 1
+    },
+    {
+        "category_id": 2,
+        "category_name": "Зарплата",
+        "user_id": 1
+    },
+    {
+        "category_id": 3,
+        "category_name": "Здоровье и фитнес",
+        "user_id": 1
+    },
+    {
+        "category_id": 4,
+        "category_name": "Кафе и рестораны",
+        "user_id": 1
+    },
+    {
+        "category_id": 5,
+        "category_name": "Машина",
+        "user_id": 1
+    },
+    {
+        "category_id": 6,
+        "category_name": "Образование",
+        "user_id": 1
+    },
+    {
+        "category_id": 7,
+        "category_name": "Платежи, комиссии",
+        "user_id": 1
+    },
+    {
+        "category_id": 8,
+        "category_name": "Покупки: одежда, техника",
+        "user_id": 1
+    },
+    {
+        "category_id": 9,
+        "category_name": "Продукты",
+        "user_id": 1
+    },
+    {
+        "category_id": 10,
+        "category_name": "Проезд",
+        "user_id": 1
+    }
+]
--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from users.models import MyUser
+
+
+class Common(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.DO_NOTHING, verbose_name='Юзер')
+    update_date = models.DateTimeField(verbose_name='Последние изменения', auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(Common):
+    """
+    Транзакция должна содержать в себе: сумму\*, время\*, категорию\*, организацию\*, описание.
+
+    """
+    category_id = models.BigAutoField(primary_key=True, verbose_name="id")
+    category_name = models.CharField(max_length=255, verbose_name="Название категории")
+    # code = models.ForeignKey(Code, on_delete=models.PROTECT, verbose_name="Шифр товара")
+    # category_parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        db_table = 'сategory'
+        verbose_name = 'Категория | Category'
+        verbose_name_plural = 'Категории | Category'
+
+    def __str__(self):
+        return self.category_name
