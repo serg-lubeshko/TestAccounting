a8b8d8c70a9b85a15e4ee8e8c262de7771281d39
lubeshkosr <lubeshkosergei@gmail.com>
2022-11-14 16:45:42 +0300

    init+app:: add swagger, app to create user

--- x1Lubeshko/settings.py
+++ x1Lubeshko/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounting.apps.AccountingConfig',
+    'rest_framework',
+    'drf_yasg',
+    'users'
 ]
 
 MIDDLEWARE = [
