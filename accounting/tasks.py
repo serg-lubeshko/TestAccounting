fca1123ed26ade9bafb35bf8364652b0c8f97036
sergei <sergei@tut.by>
2022-11-25 13:08:14 +0300

    send_email:: for educational purposes the send_mail was changed to the default from django

--- accounting/tasks.py
+++ accounting/tasks.py
@@ -1,74 +1,16 @@
-import os
-
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "x1Lubeshko.settings")
-django.setup()
-
-# from dramatiq.brokers.redis import RedisBroker
-#
-# redis_broker = RedisBroker(host="redis")
-# dramatiq.set_broker(redis_broker)
-#
-#
-# from celery.utils.log import get_task_logger
-#
-# logger = get_task_logger(__name__)
-# @app.task
-# def send_notification():
-#     subject = "Expense report"
-#     message = f"Ваш доход за прошлый день"
-#     email_from = settings.EMAIL_HOST_USER
-#     recipient_list = ['sergei_777@tut.by']
-#     send_mail(subject, message, email_from, recipient_list)
-
-
-# print(datetime.now())
-
-
-# Card.objects.create(card_name='oopop', user_id=1)
-# try:
-#     day_for_send = (datetime.utcnow() - timedelta(days=1)).date()
-#     trans_objs = Transactions.objects.values('user__username', 'user__email').annotate(
-#         sum_income=Sum('transaction_summ', filter=Q(transaction_summ__gt=0), default=0),
-#         sum_consumption=Sum('transaction_summ', filter=Q(transaction_summ__lte=0), default=0)
-#     ).filter(date_operation__date=day_for_send)
-#     count = 1
-#     Card.objects.create(card_name=str(count), user_id=1)
-#     count = count + 1
-#     for trans_obj in trans_objs:
-#         subject = "Expense report"
-#         message = f"Ваш доход за прошлый день {trans_obj['sum_income']}, а Ваши расходы составляют {trans_obj['sum_consumption']}"
-#         email_from = settings.EMAIL_HOST_USER
-#         recipient_list = [trans_obj.get('user__email', None)]
-#         send_mail(subject, message, email_from, recipient_list)
-#     return None
-# except Exception as e:
-#     Card.objects.create(card_name='oopop', user_id=1)
-#     print(e)
-
-
-# @dramatiq.actor
-# def send_notification2():
-#     print(datetime.utcnow())
-# if __name__ == '__main__':
-#     send_notification.send()
-
-
+import asyncio
 import smtplib
 import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from django.core.mail import BadHeaderError
+from django.core.mail import BadHeaderError, send_mail
 
 
 async def send_mail_user(item):
     smtp_server = "smtp.gmail.com"
-    # sender_email = "ittascompany2022@gmail.com"
-    sender_email = "efwfwefwef@gmail.com"
-    # password = "bohsksyohssunujo"
-    password = "qwqwqwqwqwqwqw"
+    sender_email = "You_email@gmail.com"
+    password = "password"
 
     msg = MIMEMultipart('alternative')
     msg['Subject'] = 'Баланс за вчерашний день)'
@@ -86,8 +28,18 @@ async def send_mail_user(item):
         mail.ehlo()
         mail.login(sender_email, password)
         mail.sendmail(sender_email, users_mail, msg.as_string())
+        send_mail(sender_email, users_mail, msg.as_string())
         mail.quit()
         return True
     except (BadHeaderError, smtplib.SMTPAuthenticationError):
         return False
+
+async def send_mail_default_django(item):
+    subject = 'BALANCE'
+    message = f"Ваш доход за прошлый день {item['sum_income']}, а Ваши расходы составляют {item['sum_consumption']}"
+    email_from = "default_company@mail.ru"
+    recipient_list = [item['user__email']]
+    # await asyncio.sleep(1)
+    send_mail(subject, message, email_from, recipient_list)
+
 
--- accounting/views/balance_views.py
+++ accounting/views/balance_views.py
@@ -1,10 +1,7 @@
-from decimal import Decimal
-
-from django.db.models import Sum, F, Prefetch
 from rest_framework import generics
 
-from accounting.models import Transactions, Card
-from accounting.serializers.card_serializers import BalanceListSerializer, BalanceCreateSerializer, \
+from accounting.models import Card
+from accounting.serializers.card_serializers import BalanceCreateSerializer, \
     BalanceListSerializer2
 from accounting.serializers.com_view import CommonCreate
 
@@ -23,15 +20,4 @@ class BalanceList(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user.pk
         return Card.objects.filter(user=user)
-
-        # return (
-        #     Transactions.objects.values(
-        #         'card',
-        #         'card__card_name',
-        #         'card__beg_balance',
-        #     ).annotate(sum_tr=Sum('transaction_summ')).filter(user=user)). \
-        #     annotate(
-        #         cur_balance=F('card__beg_balance') + F('sum_tr')
-        # )
-
 
